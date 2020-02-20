09637e5c34d3f9609ac62ae8fe64c42f315b0bf5
NoahH <noahhalig@gmsil.com>
2020-02-19 22:07:31 -0800

    Added Functionality

--- nested.py
+++ nested.py
@@ -3,20 +3,50 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "???"
+__author__ = "Noah"
 
 import sys
 
 
 def is_nested(line):
-    """Validate a single input line for correct nesting"""
-    pass
+    line = list(line)
+    i = 0
+    while i < len(line) - 1:
+        if(line[i] == "(" and line[i + 1] == "*"):
+            line[i] = "(*"
+            line.pop(i + 1)
+        elif(line[i] == "*" and line[i + 1] == ")"):
+            line[i] = "*)"
+            line.pop(i + 1)
+        i += 1
+    i = 0
+    stack = []
 
+    for index, i  in enumerate(line):
+        if(i in ["(","[","{","(*","<"]):
+            stack.append(i)
+        elif(i in [")","]","}","*)",">"]):
+            if(stack[-1] + i in ["()", "[]", "{}", "(**)", "<>"]):
+                stack.pop()
+            else:
+                return ("No " +  str(index + 1))
+    if len(stack) == 0:
+        return "Yes"
+    else:
+        return "No " + str(len(line))
 
 def main(args):
     """Open the input file and call `is_nested()` for each line"""
     # Results: print to console and also write to output file
-    pass
+    open('output.txt', 'w').close()
+    lineArray = []
+    with open("input.txt") as fp:
+        lineArray = fp.readlines()
+    for i in lineArray:
+        f = open("output.txt", "a")
+        text = is_nested(i)
+        f.write(text + "\n")
+        f.close()
 
 
 if __name__ == '__main__':
